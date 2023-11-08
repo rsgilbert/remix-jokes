ec1a780f481880f8d59f83b7b656a7eb3a09c023
SSG <gssenyonjo@gmail.com>
2023-11-08 14:40:06 +0300

    Created a parameterized route

---
+++ app/routes/jokes.$jokeId.tsx
@@ -0,0 +1,10 @@
+export default function JokeRoute() {
+    return (
+        <div>
+            <p>Here is your hilarious joke:</p>
+            <p>
+                
+            </p>
+        </div>
+    )
+}
